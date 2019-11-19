93ca83527fdfde0f619c467959349d2b8fb43cd6
ErickSilv <erick.silva241@etec.sp.gov.br>
2019-11-18 22:48:40 -0300

    Tela inicial do AspNet

---
+++ TCM_II/TCM_II/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TCM_II.MvcApplication" Language="C#" %>
---
+++ TCM_II/TCM_II/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TCM_II.Startup))]
+namespace TCM_II
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
